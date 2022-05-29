e51371e861fce48b51fe0c4dd7c0fce5c91c3a27
imam.arifin02 <imam.arifin052@gmail.com>
2022-05-29 22:14:40 +0700

    Chalange 02 Alur Login :  done

--- index.js
+++ index.js
@@ -23,19 +23,33 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  
+  let hasilLooping = [];
+  let totalPria = 0;
+  let totalWanita = 0;
+
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    hasilLooping.push(dataYangAkanDilooping[index].namaDepan + " " + dataYangAkanDilooping[index].namaBelakang);
+
+    if (dataYangAkanDilooping[index].jenisKelamin === "M") {
+      totalPria++;
+    } else {
+      totalWanita++;
+    }
+  }
+
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  let jumlahPria = totalPria;
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+  let jumlahWanita = totalWanita;
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -49,7 +63,13 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
-
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  } else if (jumlahWanita > jumlahPria) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  } else {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
   // ! JANGAN DIMODIFIKASI
   return {
     hasilLooping,
@@ -65,7 +85,7 @@ function main(data) {
   console.log(hasil.hasilLooping);
   console.log(hasil.jumlahPria);
   console.log(hasil.jumlahWanita);
-
+  console.log(hasil.komentar);
   return hasil;
 }
 
